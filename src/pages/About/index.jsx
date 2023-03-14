f24b5a31da571f4ea8c4bff43fa5c92c8d8d6556
Aliona Uzun <aliona-balti@outlook.fr>
2023-03-14 13:59:06 +0100

    définir les types de props

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -2,34 +2,14 @@
 import Header from "../../components/Header";
 import Banner from '../../components/Banner';
 import Collapse from '../../components/Collapse';
+import aboutDatas from '../../data/aboutData';
 import Footer from '../../components/Footer';
+import PropTypes from 'prop-types';
 import styled from "styled-components";
 
-export default function About() {
+export default function About({id, title, content}) {
 
-	const aboutDatas = [
-		{
-			"id": "1",
-			"title": "Fiabilité",
-			"content": "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
-		},
-		{
-			"id": "2",
-			"title": "Respect",
-			"content": "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.."
-		},
-		{
-			"id": "3",
-			"title": "Service",
-			"content": "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
-		},
-		{
-			"id": "4",
-			"title": "Sécurité",
-			"content": "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-		}
-	]
-
+	
 	return (
 		<StyledAbout>
 			<Header />
@@ -41,11 +21,17 @@ export default function About() {
 							<Collapse title={data.title} content={data.content} />
 						</div>
 					)}
-				)}
+					)}
 			</StyledMain>
 			<Footer />
 		</StyledAbout>
 	)
+}
+//je définis les types de props que je souhaite recevoir pour le composant About
+About.defaultProps = {
+	id: PropTypes.number.isRequired,
+	title: PropTypes.string.isRequired,
+	content: PropTypes.string.isRequired,
 }
 
 const StyledMain = styled.div`
