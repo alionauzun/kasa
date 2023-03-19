bd98141933a0fe491655c9f4f1701df68b3e02fb
Aliona Uzun <aliona-balti@outlook.fr>
2023-03-19 10:40:33 +0100

    mis a jour

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,4 @@
+//ce fichier est généré automatiquement par create-react-app pour mesurer les performances de l'application et afficher les résultats dans la console du navigateur 
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
